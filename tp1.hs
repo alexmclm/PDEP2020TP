7edacc433fb827f5ac2734961ea00b74ab5ebc52
Alex <alexmclm@gmail.com>
2020-05-21 17:48:59 -0300

    agregue y cambie algunas funciones, arreglar otras

--- tp1.hs
+++ tp1.hs
@@ -25,19 +25,41 @@ type PrecioPropiedad = Int
 carolina = Persona "Carolina" 500 "accionista"   [("auto",456)] [pagarAAccionista]
 manuel = Persona "Manuel" 500 "oferente singular" []  [enojarse]
 
+
+
+
+--pasarPorElBanco :: Persona -> Persona
+--pasarPorElBanco unaPersona = (aumentarDineral 40 . cambiarTactica "Comprador Compulsivo") unaPersona
+
+--aumentarDineral :: Int -> Persona -> Persona
+--aumentarDineral unDinero unaPersona = unaPersona { dinero =dinero unaPersona + unDinero}
+
+--pagarAAccionista :: Persona -> Persona
+--pagarAAccionista unaPersona | tieneTactica "accionista" unaPersona = restarDineral 100 unaPersona
+--                            | otherwise = aumentarDineral 200 unaPersona
+
+--enojarse :: Persona -> Persona
+--enojarse unaPersona = (aumentarDineral 50 . agregarAccion gritar)unaPersona
+
+-- probarValor unDinero unaPersona =  modificarDinero (+ unDinero ) unaPersona
+
 --acciones
 
-pasarPorElBanco :: Persona -> Persona
-pasarPorElBanco unaPersona = (aumentarDineral 40 . cambiarTactica "Comprador Compulsivo") unaPersona
 
-aumentarDineral :: Int -> Persona -> Persona
-aumentarDineral unDinero unaPersona = unaPersona { dinero =dinero unaPersona + unDinero}
+modificarDinero :: (Int -> Int) -> Persona -> Persona
+modificarDinero unaFuncion unaPersona = unaPersona { dinero = unaFuncion (dinero unaPersona)}
+
+
+pasarPorElBanco :: Persona -> Persona
+pasarPorElBanco unaPersona = (modificarDinero (+40) . cambiarTactica "Comprador Compulsivo") unaPersona
+
 
 cambiarTactica :: String -> Persona -> Persona
 cambiarTactica unaNuevaTactica unaPersona = unaPersona {tactica = unaNuevaTactica}
 
+
 enojarse :: Persona -> Persona
-enojarse unaPersona = (aumentarDineral 50 . agregarAccion gritar)unaPersona
+enojarse unaPersona = (modificarDinero (+50) . agregarAccion gritar)unaPersona
 
 agregarAccion :: Acciones -> Persona -> Persona
 agregarAccion unaAccion unaPersona = unaPersona {acciones = unaAccion : (acciones unaPersona)   }
@@ -53,11 +75,11 @@ tieneTactica unaTactica unaPersona = ((== unaTactica). tactica) unaPersona
 
 subastar :: Propiedad ->  Persona -> Persona
 subastar unaPropiedad unaPersona
- | tieneAlgunasTacticas unaPersona = (restarValorPropiedad unaPropiedad . agregarAdquisicion unaPropiedad ) unaPersona
+ | esTacticaOferenteOAccionista unaPersona = (restarValorPropiedad unaPropiedad . agregarAdquisicion unaPropiedad ) unaPersona
  | otherwise = id unaPersona
 
-tieneAlgunasTacticas :: Persona -> Bool
-tieneAlgunasTacticas unaPersona = tieneTactica "oferente singular" unaPersona || tieneTactica "accionista" unaPersona
+esTacticaOferenteOAccionista  :: Persona -> Bool
+esTacticaOferenteOAccionista  unaPersona = tieneTactica "oferente singular" unaPersona || tieneTactica "accionista" unaPersona
 
 restarValorPropiedad :: Propiedad -> Persona -> Persona
 restarValorPropiedad (_,valorPropiedad) unaPersona = restarDineral valorPropiedad unaPersona
@@ -65,11 +87,15 @@ restarValorPropiedad (_,valorPropiedad) unaPersona = restarDineral valorPropieda
 agregarAdquisicion :: Propiedad -> Persona -> Persona
 agregarAdquisicion unaPropiedad unaPersona = unaPersona {propiedadesCompradas = unaPropiedad : (propiedadesCompradas unaPersona) }
 
-cobrarAlquileres :: Persona -> Persona
-cobrarAlquileres unaPersona = aumentarDineral (alquileresAcobrar unaPersona) unaPersona
+--cobrarAlquileres :: Persona -> Persona
+--cobrarAlquileres unaPersona = aumentarDineral (alquileresAcobrar unaPersona) unaPersona
 
-alquileresAcobrar :: Persona -> Int
-alquileresAcobrar unaPersona =  (*10)(cantidadCasasBaratas (propiedadesCompradas unaPersona)) + (*20) (length (filter (not.esPropiedadBarata) (propiedadesCompradas unaPersona)))
+--alquileresAcobrar :: Persona -> Int
+--alquileresAcobrar unaPersona = sum.map precioAlquiler propiedadesCompradas unaPersona
+
+
+precioAlquiler unaPropiedad | esPropiedadBarata unaPropiedad = (*10)
+                            | otherwise = (*20)
 
 propiedadPrecio :: Propiedad -> Int
 propiedadPrecio unaPropiedad = snd unaPropiedad
@@ -82,14 +108,14 @@ cantidadCasasBaratas unaPropiedad = (length.filter esPropiedadBarata) unaPropied
 
 
 pagarAAccionista :: Persona -> Persona
-pagarAAccionista unaPersona | tieneTactica "accionista" unaPersona = restarDineral 100 unaPersona
-                            | otherwise = aumentarDineral 200 unaPersona
+pagarAAccionista unaPersona | tieneTactica "accionista" unaPersona = modificarDinero (+(-100)) unaPersona
+                            | otherwise = modificarDinero (+200) unaPersona
 
 restarDineral :: Int -> Persona -> Persona
 restarDineral unDinero unaPersona = unaPersona { dinero = dinero unaPersona - unDinero}
 
 hacerBerrinchePor unaPropiedad unaPersona | alcanzaDinero unaPropiedad unaPersona  = comprarPropiedad unaPropiedad unaPersona
-                                          | otherwise  = id unaPersona
+                                          | otherwise  = hacerBerrinchePor unaPropiedad unaPersona
 
 alcanzaDinero unaPropiedad unaPersona = propiedadPrecio unaPropiedad <= dinero unaPersona
 
